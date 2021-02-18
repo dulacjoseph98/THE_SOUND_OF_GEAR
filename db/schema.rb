55d8b2aa1074925b4afd7159c8c0969c3de42e25
Igor Murphy <igor.murphy@gmail.com>
2021-02-18 17:39:10 +0100

    migrate on master

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_17_105502) do
+ActiveRecord::Schema.define(version: 2021_02_18_160547) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 2021_02_17_105502) do
     t.string "address"
     t.float "latitude"
     t.float "longitude"
+    t.text "description"
     t.index ["user_id"], name: "index_devices_on_user_id"
   end
 
