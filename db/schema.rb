36755a3fb624c8f0734d07e454986c5a32f7ca73
Joseph Dulac <dulacjoseph@gmail.com>
2021-02-19 14:03:38 +0100

    done

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_18_160547) do
+ActiveRecord::Schema.define(version: 2021_02_18_152209) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,7 +47,6 @@ ActiveRecord::Schema.define(version: 2021_02_18_160547) do
     t.float "latitude"
     t.float "longitude"
     t.text "description"
-    t.integer "rating"
     t.index ["user_id"], name: "index_devices_on_user_id"
   end
 
